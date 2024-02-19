ed40d78ce3889fb12b10fa47033928ab448e3aae
Sunny Rai <sunnyrai31@gmail.com>
2024-02-20 02:45:24 +0530

    New function details added

--- src/Hooks/useStateCustom.js
+++ src/Hooks/useStateCustom.js
@@ -1,4 +1,9 @@
 import { useState } from "react";
+/**
+ * Its custom hook - which is internally using useState hook
+ * @param {*} intialValue
+ * @returns
+ */
 const useStateCustom = (intialValue) => {
   const [state, setState] = useState(intialValue);
   return [state, setState];
