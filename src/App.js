dee0003ecfacef0f3b120576f90c8301a6167d8f
sunnyrai <sunnyrai@fico.com>
2024-02-20 02:22:16 +0530

    coce refactor

--- src/App.js
+++ src/App.js
@@ -4,33 +4,32 @@ import QuestionList from "./Components/QuestionList";
 import HeaderComponent from "./Components/HeaderComponent";
 import FileUploader from "./Components/FileUploader";
 import WelcomeComponent from "./Components/WelcomeComponent";
-import {BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import AboutComponent from "./Components/AboutComponent";
 
-
 function App() {
-    return (
-        <Router>
-        <div className="App">
-            <HeaderComponent></HeaderComponent>
-            <Routes>
-                <Route path='/upload'
-                       element={<FileUploader></FileUploader>}
-                ></Route>
-                <Route path='/questionPaper'
-                       element={<QuestionList></QuestionList>}
-                ></Route>
-                <Route path='/about'
-                       element={<AboutComponent></AboutComponent>}
-                ></Route>
-                <Route path='/'
-                       element={<WelcomeComponent></WelcomeComponent>}
-                ></Route>
-            </Routes>
-
-        </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <HeaderComponent></HeaderComponent>
+        <Routes>
+          <Route path="/upload" element={<FileUploader></FileUploader>}></Route>
+          <Route
+            path="/questionPaper"
+            element={<QuestionList></QuestionList>}
+          ></Route>
+          <Route
+            path="/about"
+            element={<AboutComponent></AboutComponent>}
+          ></Route>
+          <Route
+            path="/"
+            element={<WelcomeComponent></WelcomeComponent>}
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
