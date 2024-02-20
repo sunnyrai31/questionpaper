14a9ce11c3329ce242b9d18f75ef50fc17311941
Sunny Rai <sunnyrai31@gmail.com>
2024-02-21 04:22:16 +0530

    Routing modularized, code cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import HeaderComponent from "./Components/HeaderComponent";
 import { BrowserRouter as Router } from "react-router-dom";
-
 import { useEffect } from "react";
 import Routing from "./Routing";
 
@@ -9,16 +8,6 @@ function App() {
   useEffect(() => {
     // document.title = process.env.REACT_APP_TITLE;
   }, []);
-  // const routingDetails = [
-  //   { path: "/", componentName: WelcomeComponent },
-  //   { path: "/about", componentName: AboutComponent },
-  //   { path: "/QuestionQuiz", componentName: QuestionQuiz },
-  //   { path: "/SelectOnDemand", componentName: SelectOnDemand },
-  //   { path: "/UseState", componentName: UseStateHook },
-  //   { path: "/hooksUsestate", componentName: UseStateHook },
-  //   { path: "/hooksUseEffect", componentName: UseEffectHook },
-  //   { path: "/hooksUseStateCustom", componentName: UseStateCustomHook },
-  // ];
   return (
     <Router>
       <div className="App">
